2d2025400cb2c2118eb360e11c988447b86f908b
Rachana Khatri <kcs24b.rcn@ismt.edu.np>
2025-12-09 15:38:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeColor() {
+    let name = document.getElementById("name");
+
+    if (name.style.color === "blue") {
+        name.style.color = "black";
+    } else {
+        name.style.color = "blue";
+    }
+}
